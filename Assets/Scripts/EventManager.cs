1691e1e8d29423315978e36f3834b590e6083cd6
Hao <1719598193@qq.com>
2024-07-27 23:54:03 +0800

    事件管理

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum EventType
+{
+
+}
+public class EventManager : MonoBehaviour
+{
+    public static EventManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public Dictionary<EventType, UnityEvent> events = new Dictionary<EventType, UnityEvent>();
+    public void AddEvent(EventType type, UnityAction e)
+    {
+        if (!events.ContainsKey(type)) events.Add(type, new());
+        events[type].AddListener(e);
+    }
+    public void RemoveEvent(EventType type, UnityAction e)
+    {
+        if (!events.ContainsKey(type)) return;
+        events[type].RemoveListener(e);
+    }
+
+    public void TriggerEvent(EventType type)
+    {
+        if (!events.ContainsKey(type)) return;
+        events[type].Invoke();
+    }
+}
